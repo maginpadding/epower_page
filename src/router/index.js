e863820f65dd5a494f541f1fc7833b81d6de0549
marginpadding <1824966501@qq.com>
2018-07-31 20:40:50 +0800

    提交

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import pcTradeInfo from '@/components/pc_trade_info'
+import mobileTradeInfo from '@/components/mobile_trade_info'
 
 Vue.use(Router)
 
@@ -8,8 +9,12 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
+      name: 'pcTradeInfo',
+      component: pcTradeInfo
+    },{
+        path: '/mobile',
+        name: 'mobileTradeInfo',
+        component: mobileTradeInfo
+      }
   ]
 })
